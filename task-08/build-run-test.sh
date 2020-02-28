ef691f6feb3d5ee2b22262eb4c6c5bddb055d59c
PavelCoup <pavel.coup@gmail.com>
2020-02-28 13:57:00 +0300

    build-run-test.sh

--- task-08/build-run-test.sh
+++ task-08/build-run-test.sh
@@ -4,4 +4,3 @@ docker build -t wcg -f `dirname $0`/dockerfile.multi .
 docker run --rm -d --name small-wcg -p 80:8888 wcg
 curl -s -H "Content-Type: application/json" -d '{"text":"ths is a really really really important thing this is"}' http://127.0.0.1/version
 
-
